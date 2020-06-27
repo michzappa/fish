23ffc747f225eb35d8c1cbefed1252d030e28d7f
michzappa <zapprich@gmail.com>
2020-06-27 11:22:39 -0500

    corrected path to static react files

--- app.js
+++ app.js
@@ -19,6 +19,6 @@ app.use(express.static(path.join(__dirname, "public")));
 app.use("/rooms", roomsRouter);
 
 // displays the react page
-app.use(express.static(path.join(__dirname, "../frontend/build")));
+app.use(express.static(path.join(__dirname, "./frontend/build")));
 
 module.exports = app;
